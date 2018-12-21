c044e6da9a0a3dbd3a00d551065eef60e72428f3
Assad.Ul-Hassan <Assad.Ul-Hassan@HE20217.pitguk.com>
2018-12-21 18:02:10 +0500

    abc

---
+++ assignment/src/main/java/com/starzplay/assignment/AssignmentApplication.java
@@ -0,0 +1,17 @@
+package com.starzplay.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages="com.starzplay.assignment.controller")
+public class AssignmentApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentApplication.class, args);
+	}
+
+}
+
